838cc32a5014656bd69bcb63bd1691ed9a59cc09
Sourcery AI <>
2022-11-16 02:44:29 +0000

    'Refactored by Sourcery'

--- searchpass.py
+++ searchpass.py
@@ -4,27 +4,24 @@ import hashlib
 from getpass import getpass
 
 def indexfile_dict(indexfilename):
-    indexfilehandle = open(indexfilename,'rt')
-    dict = yaml.safe_load(indexfilehandle.read())
-    indexfilehandle.close()
+    with open(indexfilename,'rt') as indexfilehandle:
+        dict = yaml.safe_load(indexfilehandle.read())
     digits = 0
     for item in dict:
-        if item[0:8].lower() == 'indexes_':
+        if item[:8].lower() == 'indexes_':
             dict = dict[item][0]
             digits = int(item[8])
     return dict, digits
 
 def ask_password():
-    password = getpass('Password? (Just press Enter to quit) ')
-    return password
+    return getpass('Password? (Just press Enter to quit) ')
 
 def sha1sum_password(password):
     hash = hashlib.sha1(password.encode('utf-8'))
-    sha = hash.hexdigest().upper()
-    return sha
+    return hash.hexdigest().upper()
 
 def segment(dict, sha, digits):
-    fewdigits = sha[0:digits]
+    fewdigits = sha[:digits]
     begin = dict[fewdigits]["begin_byte"]
     length = dict[fewdigits]["byte_count"]
 
@@ -35,44 +32,46 @@ def read_segment(hashfilehandle, sha, begin, length, digits):
     end = begin + length
     pos = hashfilehandle.tell()
     match = False
-    while pos < end and match == False:
+    while pos < end and not match:
         line = hashfilehandle.readline()
         pos = hashfilehandle.tell()
-        if sha[0:digits].upper() != line[0:digits].upper():
-            print("Error: Need to run index_hashfile.py or there is a bug. Searching for " + sha[0:digits].upper() + " on line: ")
+        if sha[:digits].upper() != line[:digits].upper():
+            print(
+                f"Error: Need to run index_hashfile.py or there is a bug. Searching for {sha[:digits].upper()} on line: "
+            )
+
             print(line)
             break
-        if sha.upper() == line[0:40].upper():
+        if sha.upper() == line[:40].upper():
             match = True
             print("Match found:")
             print(line)
             break
-    if match == False:
+    if not match:
         print("Match not found.\n")
 
 def main():
     indexfilename = 'pwned-passwords-hash-index_v8.yaml'
     hashlistfilename = 'pwned-passwords-sha1-ordered-by-hash-v8.txt'
-    hashfilehandle = open(hashlistfilename,'rt')
-    print("Reading " + indexfilename)
-    dict = indexfile_dict(indexfilename)
-    digits = dict[1]
-    dict = dict[0]
-    # print(dict)
-    print("")
+    with open(hashlistfilename,'rt') as hashfilehandle:
+        print(f"Reading {indexfilename}")
+        dict = indexfile_dict(indexfilename)
+        digits = dict[1]
+        dict = dict[0]
+        # print(dict)
+        print("")
 
-    password = " "
-    while len(password) > 0:
-        password = ask_password()
-        if len(password) < 1:
-            break
-        sha = sha1sum_password(password)
-        # password = ""
-        print("Looking for SHA1SUM: " + sha)
-        begin = segment(dict, sha, digits)[0]
-        length = segment(dict, sha, digits)[1]
-        read_segment(hashfilehandle, sha, begin, length, digits)
-    hashfilehandle.close()
+        password = " "
+        while password != "":
+            password = ask_password()
+            if len(password) < 1:
+                break
+            sha = sha1sum_password(password)
+                    # password = ""
+            print(f"Looking for SHA1SUM: {sha}")
+            begin = segment(dict, sha, digits)[0]
+            length = segment(dict, sha, digits)[1]
+            read_segment(hashfilehandle, sha, begin, length, digits)
 
 if __name__ == "__main__":
     main()
